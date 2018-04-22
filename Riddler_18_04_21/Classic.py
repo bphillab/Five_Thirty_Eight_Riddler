ea335f95da2ab72dd59a28b68d185aae72e38ed7
bphillab <bphillab@gmail.com>
2018-04-22 14:07:41 -0500

    adding in solution to Classic

--- Riddler_18_04_21/Classic.py
+++ Riddler_18_04_21/Classic.py
@@ -13,6 +13,7 @@ def simulate_cross(omega, num_times):
     """
     times_cross = []
     times_dont_cross = []
+    dont_cross_correct = []
     i = 0
     while i < num_times:
         # Decide to cross
@@ -29,26 +30,34 @@ def simulate_cross(omega, num_times):
         t5 = 0
         t6 = 0
         t7 = np.maximum(np.ceil(np.random.uniform(-t,t,1)),[0])[0]
+        cross_time = t1+t3+t4
         times_cross = times_cross + [t1+t3+t4]
         if vert_or_horiz == 1:
+            dont_cross_time = t2 + t6 + t7
             times_dont_cross = times_dont_cross + [t2 + t6 + t7]
         if vert_or_horiz == 0:
+            dont_cross_time = t2 + t5 + t4
             times_dont_cross = times_dont_cross + [t2 + t5 + t4]
-    return np.mean(times_cross), np.mean(times_dont_cross)
+        if cross_time > dont_cross_time:
+            dont_cross_correct = dont_cross_correct+[1]
+        else:
+            dont_cross_correct = dont_cross_correct+[0]
+    return np.mean(times_cross), np.mean(times_dont_cross), np.mean(dont_cross_correct)
 
 
 if __name__ == '__main__':
     print('Starting at 1, trying until cross threshold:')
     i = 1
-    tempx, tempy = simulate_cross(i, 10000)
+    tempx, tempy, percent_right = simulate_cross(i, 10000)
     while tempx < tempy:
         i = i+1
-        tempx, tempy = simulate_cross(i, 10000)
+        tempx, tempy, percent_right = simulate_cross(i, 10000)
 
     print('Crossed threshold at i = ', i)
     print('Diagnostics: ')
     print(tempx)
     print(tempy)
+    print(percent_right)
     print('Exact solution based on harmonic number gives:')
     print('2.09069')
     print('2.04534')
