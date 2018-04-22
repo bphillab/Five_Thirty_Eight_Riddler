a23db9056fffee9a3f57a4c74ca9a477725e8864
bphillab <bphillab@gmail.com>
2018-04-22 12:12:54 -0500

    adding in solution to Classic

--- Riddler_18_04_21/Classic.py
+++ Riddler_18_04_21/Classic.py
@@ -1,13 +1,8 @@
 import numpy as np
 
 
-def get_T(omega):
-    p = np.random.uniform(0, 1, 1)
-    normalizer = sum([1/i for i in range(1,omega+1)])
-    for i in range(1,omega+1):
-        if p >= sum([1/j for j in range(1,i)])/normalizer and p <sum([1/j for j in range(1,i+1)])/normalizer:
-            return i
-
+def get_t(omega):
+    return np.floor(np.random.uniform(1, omega+1, 1))[0]
 
 def simulate_cross(omega, num_times):
     """
@@ -15,23 +10,40 @@ def simulate_cross(omega, num_times):
     :param num_times: number of iterations to run
     :return: Average waiting time before making it to restaurant
     """
-    times = []
-    for i in range(num_times):
+    times_cross = []
+    times_dont_cross = []
+    i = 0
+    while i < num_times:
         # Decide to cross
-        temp_time = 0
-        T = get_T(omega)
-        t_obs = np.average([1]+ np.ceil(np.random.uniform(1, T, 2)))
-        t_this = np.max(np.append(np.ceil(np.random.uniform(-T, T, 1)),0))
-        if t_this < t_obs:
-            times = times + [t_this]
-        else:
-            times = times + [np.max(np.append(np.ceil(np.random.uniform(-T, T, 1)),0))]
+        t = get_t(omega)
+        if np.ceil(np.random.uniform(0,t,1))[0] != 1:
+            continue
 
-    return times
+        vert_or_horiz = np.random.choice(range(2),1)[0]
+        i = i + 1
+        t1 = 0
+        t2 = 1
+        t3 = np.maximum(np.ceil(np.random.uniform(-t,t,1)),0)[0]
+        t4 = np.maximum(np.ceil(np.random.uniform(-t,t,1)),[0])[0]
+        t5 = 0
+        t6 = 0
+        t7 = np.maximum(np.ceil(np.random.uniform(-t,t,1)),[0])[0]
+        times_cross = times_cross + [t1+t3+t4]
+        if vert_or_horiz == 1:
+            times_dont_cross = times_dont_cross + [t2 + t6 + t7]
+        if vert_or_horiz == 0:
+            times_dont_cross = times_dont_cross + [t2 + t5 + t4]
+    return np.mean(times_cross), np.mean(times_dont_cross)
 
-print("Trying until I reach 1: ")
-omega = 1
-while(np.mean(simulate_cross(omega, 10000))< 1):
-    omega = omega+1
+if __name__ == '__main__':
+    print('Starting at 1, trying until cross threshold:')
+    i = 1
+    tempx, tempy = simulate_cross(i, 10000)
+    while tempx < tempy:
+        i = i+1
+        tempx, tempy = simulate_cross(i, 10000)
 
-print("Breaks 1 at: ", omega)
+    print('Crossed threshold at i = ', i)
+    print('Diagnostics: ')
+    print(tempx)
+    print(tempy)
