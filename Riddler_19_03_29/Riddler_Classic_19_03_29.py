d984f42a1178b1ab36961229d592d5598e953155
Benjamin Phillabaum <bphillab@gmail.com>
2019-03-29 18:11:21 -0700

    New riddler!

---
+++ Riddler_19_03_29/Riddler_Classic_19_03_29.py
@@ -0,0 +1,35 @@
+"""
+From Steven Pratt, ordinal bee probability:
+
+You are competing in a spelling bee alongside nine other contestants.
+You can each spell words perfectly from a certain portion of the
+dictionary but will misspell any word not in that portion of the book.
+Specifically, you have 99 percent of the dictionary down cold, and your
+opponents have 98 percent, 97 percent, 96 percent, and so on down to
+90 percent memorized. The bee’s rules are simple: The contestants take
+turns spelling in some fixed order, which then restarts with the first
+surviving speller at the end of a round. Miss a word and you’re out,
+and the last speller standing wins. The bee words are chosen randomly
+from the dictionary.
+
+First, say the contestants go in decreasing order of their knowledge,
+so that you go first. What are your chances of winning the spelling bee?
+Second, say the contestants go in increasing order of knowledge, so that
+you go last. What are your chances of winning now?
+"""
+
+from copy import deepcopy
+import numpy as np
+
+
+def simulate_bee(probas: list) -> float:
+    contestants = deepcopy(probas)
+    i = 0
+    while len(contestants) > 1:
+        if contestants[i] < np.random.uniform(0,1):
+            contestants.pop(i)
+        i = (i+1)%len(contestants)
+    return contestants[0]
+
+
+
