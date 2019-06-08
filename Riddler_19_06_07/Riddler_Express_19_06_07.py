60181c8834985dba05f6936f5fb8f3e64ff009ca
Benjamin Phillabaum <bphillab@gmail.com>
2019-06-07 17:28:54 -0700

    New riddler

---
+++ Riddler_19_06_07/Riddler_Express_19_06_07.py
@@ -0,0 +1,34 @@
+"""From Marcus Farbstein and Mark Baird, a logical mystery story, with your very freedom hanging in the balance:
+
+You’re super rich, and you often joke with your cadre of intelligent friends and family about getting kidnapped. You all
+agree that if you were ever kidnapped, the evildoers would knock you out so stealthily that you’d never feel the blow.
+Then, one snowy night, you step out of a restaurant and, just as predicted, never feel the blow.
+
+When you stir back into consciousness, it’s night, but it’s not snowy. You find yourself sitting on a beach. The sky is
+clear, with no moon. In front of you stands a shadowy figure whose face you can’t make out. He throws a blocky
+rectangular object at your feet. “That’s a satellite phone,” the figure growls. “It’s got one minute of battery left in
+it. Use that to call your people to let ’em know you’re not dead — but not until daylight.” He tosses a paper bag next
+to the sat phone. “That’s some sandwiches and water, enough for a few days. That’s salt,” he explains, waving toward the
+surf. “If your people pay our ransom,” he continues, “We’ll come get you. Otherwise, there won’t be any more paper bags.
+Remember, wait until daylight to make that call.”
+
+He then turns and climbs into a dinghy in the light surf, starts its outboard motor and zooms away. All this time you’ve
+been too groggy to do anything but listen. Now you watch as the dinghy disappears into the gloom, its wake a faint wash
+of phosphorescence that quickly fades. Later, there’s a bare wink of lights at the horizon, presumably the mothership
+getting underway and leaving.
+
+Even though it’s a moonless night, there’s sufficient starlight to assess your surroundings. Your grogginess is gone and
+you walk about. You’re on a tiny island, which you estimate is a bit more than a mile by half a mile. There are no
+trees; it’s all flat sand. You taste the water rolled up by the surf, and it is indeed salt. The air is cool, but not
+cold. Your wallet, expensive chronometer, keys, cell phone, jewelry and small change are all gone; all you have are the
+clothes on your back — even your shoes and socks have been taken. The bag contains four sandwiches, all liverwurst with
+peanut butter on cheap rye bread, and four one-pint bottles of water. No napkins. Your knowledge of astronomy is too
+weak to try to estimate your location by the stars, but you’re not stupid. Before daybreak, you’ve worked out exactly
+how you’ll use that minute of time on the satellite phone so that your people, who are also not stupid, will be able to
+dispatch rescue.
+
+What will you say?"""
+
+"""It appears that this problem is not solved via a calculation per se. I imagine there will be many solutions around 
+finding the longitude and latitude based on when dawn occurs and some other facts. 
+"""
