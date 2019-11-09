be15a302c3303f85fe1379ac72002d3297d6ac03
Benjamin Phillabaum <bphillab@gmail.com>
2019-11-08 16:58:48 -0800

    New riddles and solutions

--- Riddler_19_11_01/Riddler_Classic_19_11_01.py
+++ Riddler_19_11_01/Riddler_Classic_19_11_01.py
@@ -1,8 +1,12 @@
+import sys
+from math import e
+
 import numpy as np
 from scipy.special import binom
 
 
 class Memoize:
+    '''Stolen memoization code'''
     def __init__(self, f):
         self.f = f
         self.memo = {}
@@ -15,6 +19,7 @@ class Memoize:
 
 
 def get_partition_function(current_rank, so_far, total):
+    '''Essentially something to let me gather the probability distribution for adding to rank'''
     remaining = total - so_far
     return np.array(
         [binom(current_rank + i, i) * binom(total - current_rank - i - 1, remaining - i) for i in range(remaining + 1)])
@@ -22,6 +27,7 @@ def get_partition_function(current_rank, so_far, total):
 
 @Memoize
 def score_stop(current_rank, so_far, total):
+    '''Score if you stop'''
     partition_function = get_partition_function(current_rank, so_far, total)
     denom = sum(partition_function)
     expected_value = -sum([partition_function[i] * (current_rank + i) / denom for i in range(len(partition_function))])
@@ -30,6 +36,7 @@ def score_stop(current_rank, so_far, total):
 
 @Memoize
 def optimal_decider(current_rank, so_far, total):
+    '''At each iteration decide to continue or stop'''
     remaining = total - so_far
     if remaining == 0:
         return -current_rank
@@ -45,8 +52,9 @@ def optimize_all(num_suitor):
 
 
 def main(num_suit):
+    sys.setrecursionlimit(num_suit * 10)
     print("For ", num_suit, " suitors the minimal rank score is: ", optimize_all(num_suit))
-
+    print("Compare for large in with ", 1 + e)
 
 if __name__ == '__main__':
     main(10)
---
+++ Riddler_19_11_08/Riddler_Classic_19_11_08.py
@@ -0,0 +1,33 @@
+"""This week’s Riddler Classic comes to us from Oren, who asked not to be further identified:
+
+Now that Halloween has come and gone, your chances of getting free candy have similarly disappeared. Desperate for
+sugar, you wander into the candy store, where three kinds of candy are being sold: Almond Soys (yummy, sounds vegan!),
+Butterflingers and Candy Kernels.
+
+You’d like to buy at least one candy and at most 100, but you don’t care precisely how many you get of each or how many
+you get overall. So you might buy one of each, or you might buy 30 Almond Soys, six Butterflingers and 64 Candy Kernels.
+As long as you have somewhere between one and 100 candies, you’ll leave the store happy.
+
+But as a member of Riddler Nation, you can’t help but wonder: How many distinct ways are there for you to make your
+candy purchase?"""
+
+"""
+Solution: This is a classic 'stars-and-bars' problem. The solution is to consider 4 types of candy and 100 candies to be
+distributed. The fourth type would be a 'trash/non-realized' candy. One issue is that this allows for all trash so we 
+subtract one for that.
+
+Stars-and-bars has Binom(N+M,M) - Binom(min+M,min)
+
+
+"""
+
+from scipy.special import binom
+
+
+def stars_and_bars(num_candy, num_types, min_candy):
+    return int(binom(num_candy + num_types + 1 - 1, num_types) - binom(min_candy - 1 + num_types + 1 - 1, num_types))
+
+
+if __name__ == '__main__':
+    print("Number of ways for 100 candies, 3 kinds of candies, minimum of one candy selected: ",
+          stars_and_bars(100, 3, 1))
