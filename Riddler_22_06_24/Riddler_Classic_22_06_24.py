5bf2e41f5cfb3125613f58b4f6cef93744b1188e
Benjamin Phillabaum <bphillab@gmail.com>
2022-06-26 19:37:12 -0400

    New riddles and solutions
    Signed-off-by: Benjamin Phillabaum <bphillab@gmail.com>

--- Riddler_22_06_24/Riddler_Classic_22_06_24.py
+++ Riddler_22_06_24/Riddler_Classic_22_06_24.py
@@ -1,3 +1,6 @@
+from scipy.special import binom
+
+
 def goatcheck(goats):
     tempgoats = goats
     numgoats = len(goats)
@@ -8,8 +11,7 @@ def goatcheck(goats):
     return 0
 
 
-if __name__ == "__main__":
-    nmg = 9
+def brute_force(nmg):
     cnt = 0
     for a1 in range(nmg):
         for a2 in range(nmg):
@@ -21,4 +23,51 @@ if __name__ == "__main__":
                                 for a8 in range(nmg):
                                     for a9 in range(nmg):
                                         cnt += goatcheck([a1, a2, a3, a4, a5, a6, a7, a8, a9])
-    print(cnt)
+    return cnt
+
+
+def multinomial(params):
+    if len(params) == 1:
+        return 1
+    return binom(sum(params), params[-1]) * multinomial(params[:-1])
+
+
+def slight_improvement(nmg):
+    cnt = 0
+    for a1 in range(nmg + 1):
+        for a2 in range(nmg + 1):
+            if a1 + a2 > nmg:
+                continue
+            for a3 in range(nmg + 1):
+                if a1 + a2 + a3 > nmg:
+                    continue
+                for a4 in range(nmg + 1):
+                    if a1 + a2 + a3 + a4 > nmg:
+                        continue
+                    for a5 in range(nmg + 1):
+                        if a1 + a2 + a3 + a4 + a5 > nmg:
+                            continue
+                        for a6 in range(nmg + 1):
+                            if a1 + a2 + a3 + a4 + a5 + a6 > nmg:
+                                continue
+                            for a7 in range(nmg + 1):
+                                if a1 + a2 + a3 + a4 + a5 + a6 + a7 > nmg:
+                                    continue
+                                for a8 in range(nmg + 1):
+                                    if a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 > nmg:
+                                        continue
+                                    for a9 in range(nmg + 1):
+                                        if a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 != nmg:
+                                            continue
+                                        if goatcheck(
+                                                a1 * [0] + a2 * [1] + a3 * [2] + a4 * [3] + a5 * [4] + a6 * [5] + a7 * [
+                                                    6] + a8 * [7] + a9 * [
+                                                    8]):
+                                            cnt += multinomial([a1, a2, a3, a4, a5, a6, a7, a8, a9])
+    return cnt
+
+
+if __name__ == "__main__":
+    num_goats = 9
+    count = slight_improvement(num_goats)
+    print(count)
